2ba9d15f6cac6925a99d2c1fd0f983d7982f3dfe
Stephan Ude <ude@mediatis.de>
2023-11-29 20:59:10 +0100

    use standardised backend field type json

--- Classes/Backend/DataHandler/MetaDataHandler.php
+++ Classes/Backend/DataHandler/MetaDataHandler.php
@@ -5,6 +5,7 @@ namespace DigitalMarketingFramework\Typo3\Distributor\Core\Backend\DataHandler;
 use DigitalMarketingFramework\Distributor\Core\Factory\QueueDataFactoryInterface;
 use DigitalMarketingFramework\Typo3\Distributor\Core\Domain\Model\Queue\Job;
 use DigitalMarketingFramework\Typo3\Distributor\Core\Registry\Registry;
+use JsonException;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\SingletonInterface;
 
@@ -24,19 +25,13 @@ class MetaDataHandler implements SingletonInterface
     protected function updateJobData(array &$fieldArray): void
     {
         $job = new Job();
-        $serializedData = json_decode($fieldArray['serialized_data'] ?? '', null, 512, JSON_THROW_ON_ERROR);
-        if (!(bool)$serializedData) {
-            $job->setSerializedData('');
-        } else {
-            $job->setSerializedData(json_encode($serializedData, JSON_THROW_ON_ERROR));
+        $job->setSerializedData($fieldArray['serialized_data']);
+
+        $hash = $this->queueDataFactory->getJobHash($job);
+        $job->setHash($hash);
+        if ($hash !== 'undefined') {
+            $fieldArray['hash'] = $job->getHash();
         }
-
-        $job->setHash($fieldArray['hash'] ?? '');
-
-        $fieldArray['serialized_data'] = json_encode(json_decode($job->getSerializedData(), null, 512, JSON_THROW_ON_ERROR), JSON_THROW_ON_ERROR);
-
-        $job->setHash($this->queueDataFactory->getJobHash($job));
-        $fieldArray['hash'] = $job->getHash();
 
         $label = $this->queueDataFactory->getJobLabel($job);
         $job->setLabel($label);
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,6 @@
 <?php
 
 use DigitalMarketingFramework\Typo3\Distributor\Core\Backend\DataHandler\MetaDataHandler;
-use DigitalMarketingFramework\Typo3\Distributor\Core\Form\Element\JsonFieldElement;
 use DigitalMarketingFramework\Typo3\Distributor\Core\Scheduler\QueueCleanupFieldProvider;
 use DigitalMarketingFramework\Typo3\Distributor\Core\Scheduler\QueueCleanupTask;
 use DigitalMarketingFramework\Typo3\Distributor\Core\Scheduler\QueueProcessorFieldProvider;
@@ -16,24 +15,19 @@ defined('TYPO3') || die();
         'module.tx_form.settings.yamlConfigurations.1673273385 = EXT:dmf_distributor_core/Configuration/Yaml/FormSetup.yaml',
     ];
     ExtensionManagementUtility::addTypoScriptSetup(implode(PHP_EOL, $typoScripts));
+
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][QueueProcessorTask::class] = [
         'extension' => 'dmf_distributor_core',
         'title' => 'Digital Marketing Framework - Distributor - Queue Worker',
         'description' => 'Processes the next batch of form submissions using the digital-marketing-framework/distributor',
         'additionalFields' => QueueProcessorFieldProvider::class,
     ];
+
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][QueueCleanupTask::class] = [
         'extension' => 'dmf_distributor_core',
         'title' => 'Digital Marketing Framework - Distributor - Queue Cleanup',
         'description' => 'Removes old submissions from the database to be compliant with data protection regulations',
         'additionalFields' => QueueCleanupFieldProvider::class,
-    ];
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = MetaDataHandler::class;
-    // Add textarea with built-in json formatting
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1673431342] = [
-        'nodeName' => 'digitalMarketingFrameworkJsonFieldElement',
-        'priority' => 40,
-        'class' => JsonFieldElement::class,
     ];
 
     // backend module configuration
@@ -48,4 +42,5 @@ defined('TYPO3') || die();
         'setup',
         "@import 'EXT:dmf_distributor_core/Configuration/TypoScript/setup.typoscript'"
     );
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = MetaDataHandler::class;
 })();
