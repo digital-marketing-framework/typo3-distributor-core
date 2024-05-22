896317feb49d3320b2485af4bae913a0cf1a847e
Stephan Ude <ude@mediatis.de>
2024-05-22 21:50:34 +0200

    refactoring (end point storage)

--- ext_tables.sql
+++ ext_tables.sql
@@ -11,12 +11,3 @@ CREATE TABLE tx_dmfdistributorcore_domain_model_queue_job (
   changed int(11) unsigned DEFAULT '0' NOT NULL,
   created int(11) unsigned DEFAULT '0' NOT NULL
 );
-
-CREATE TABLE tx_dmfdistributorcore_domain_model_api_endpoint (
-	name varchar(64) DEFAULT '',
-	enabled tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	disable_context tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	allow_context_override tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	expose_to_frontend tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	configuration_document text DEFAULT '',
-);
