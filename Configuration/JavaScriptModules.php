d0a3f1b76fe6f015a5f1cb1706753e5e7de92b52
Stephan Ude <ude@mediatis.de>
2025-05-14 17:17:09 +0200

    [TASK] TYPO3 13 update (MFR-136)

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dependencies' => ['form'],
+    'imports' => [
+        '@digital-marketing-framework/distributor-core/' => 'EXT:dmf_distributor_core/Resources/Public/JavaScript/',
+    ],
+];
