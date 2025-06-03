863019b0e86e116a53b3435319597fb635606386
Stephan Ude <ude@mediatis.de>
2025-06-03 18:32:50 +0200

    version increase

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,13 +7,13 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '3.0.2',
+    'version' => '3.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
             'form' => '12.4.0-13.4.99',
             'form_fieldnames' => '3.5.0-4.99.99',
-            'dmf_core' => '3.0.0-3.99.99',
+            'dmf_core' => '3.0.3-3.99.99',
             'dmf_template_engine_twig' => '3.0.0-3.99.99',
         ],
         'conflicts' => [
