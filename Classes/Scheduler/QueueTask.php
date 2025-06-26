cf0bb94ff50eb212cc87acdb4e4e8b50b873a47a
Stephan Ude <ude@mediatis.de>
2025-06-26 09:19:26 +0200

    [FEATURE] Introducing Cleanup Tasks

--- Classes/Scheduler/QueueTask.php
+++ Classes/Scheduler/QueueTask.php
@@ -3,7 +3,9 @@
 namespace DigitalMarketingFramework\Typo3\Distributor\Core\Scheduler;
 
 use DigitalMarketingFramework\Core\Queue\QueueInterface;
+use DigitalMarketingFramework\Core\Queue\QueueProcessorInterface;
 use DigitalMarketingFramework\Distributor\Core\Registry\RegistryInterface;
+use DigitalMarketingFramework\Distributor\Core\Service\DistributorInterface;
 use DigitalMarketingFramework\Typo3\Core\Registry\RegistryCollection;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Scheduler\Task\AbstractTask;
@@ -14,10 +16,17 @@ abstract class QueueTask extends AbstractTask
 
     protected QueueInterface $queue;
 
+    protected QueueProcessorInterface $queueProcessor;
+
+    protected DistributorInterface $distributor;
+
     protected function prepareTask(): void
     {
         $registryCollection = GeneralUtility::makeInstance(RegistryCollection::class);
         $this->registry = $registryCollection->getRegistryByClass(RegistryInterface::class);
-        $this->queue = $this->registry->getPersistentQueue();
+
+        $queue = $this->registry->getPersistentQueue();
+        $distributor = $this->registry->getDistributor();
+        $this->queueProcessor = $this->registry->getQueueProcessor($queue, $distributor);
     }
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -7,13 +7,13 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '3.1.1',
+    'version' => '3.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
             'form' => '12.4.0-13.4.99',
             'form_fieldnames' => '3.5.0-4.99.99',
-            'dmf_core' => '3.1.1-3.99.99',
+            'dmf_core' => '3.3.0-3.99.99',
             'dmf_template_engine_twig' => '3.0.0-3.99.99',
         ],
         'conflicts' => [
